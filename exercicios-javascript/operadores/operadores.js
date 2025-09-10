f2f69b5914f74bd3aa4574e49b2df504e7135964
Allyson <2000638+allysonsouza@users.noreply.github.com>
2025-09-10 16:44:00 -0300

    Base do repositório

---
+++ exercicios-javascript/operadores/operadores.js
@@ -0,0 +1,14 @@
+// Saída no console de uma string
+console.log('Exibir mensagem no console do navegador!');
+
+// Saída no console de uma operação
+console.log(10 + 9);
+
+// Saída no console de uma variável
+let nomeDoFuncionario = 'Kaio';
+let diaDeTrabalho = 10;
+
+console.log(nomeDoFuncionario);
+
+// Concatenação de strings (ou, soma de strings)
+console.log('Olá ' + nomeDoFuncionario + ', seja bem-vindo, ao seu dia de trabalho número ' + diaDeTrabalho);
---
+++ exercicios-javascript/variaveis/main.js
@@ -0,0 +1,22 @@
+/*
+Tudo entre os comandos é um comentário.
+Podemos ter múltiplas linhas.
+*/
+alert(40 + ' Alunos');
+
+let minhaVariavel; // Declaração de variável
+
+minhaVariavel = -10.5;
+
+alert(minhaVariavel);
+
+let outraVariavel = 'Aprendendo JavaScript';
+
+alert(outraVariavel);
+
+// Comentário de linha única
+
+let nomeDoAluno = 'Alan';
+
+
+
