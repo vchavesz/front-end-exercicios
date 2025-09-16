e8bc3fbd7a34d0a60985c43287191dc77d9f9efb
vchavesz <viniciuschaves1007@gmail.com>
2025-09-16 19:57:16 -0300

    Update exer4.js

--- Vinicius/exer4.js
+++ Vinicius/exer4.js
@@ -4,5 +4,29 @@
 // //Faça um script em JavaScript que solicite a experiência e a formação do candidato, e calcule e exiba qual seria a sua pontuação para o concurso.
 
 
-let experiencia = 5;
-let 
+let experiencia = prompt("Quantos anos de experiência você tem?");
+let formacao = prompt("Qual sua formação? (superior, pos, mestrado, doutorado)");
+
+let pontos = 0;
+
+// Experiência
+if (experiencia < 5) {
+  pontos += 10;
+} else {
+  pontos += 20;
+}
+
+// Formação
+if (formacao === "superior") {
+  pontos += 10;
+} else if (formacao === "pos") {
+  pontos += 20;
+} else if (formacao === "mestrado") {
+  pontos += 30;
+} else if (formacao === "doutorado") {
+  pontos += 40;
+} else {
+  alert("Formação inválida.");
+}
+
+alert("Sua pontuação total é: " + pontos);
