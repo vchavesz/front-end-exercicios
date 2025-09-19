bc95c1724168d0a7a375712ef6dda05ce08a6a43
vchavesz <viniciuschaves1007@gmail.com>
2025-09-18 21:00:41 -0300

    Update exer8.js

--- exercicios-aluno/Vinicius/exer8.js
+++ exercicios-aluno/Vinicius/exer8.js
@@ -1,15 +1,12 @@
-// crie um while que conte dso 1 ao 10 5 vezes
-let num = 1;
-
-while(num){
-
-   \
-    if(num <= 10){
-    console.log(num);
-    num++;
-
-    }
-
-}
+// 1. Escreva um programa que solicite 1 ou 0. Caso o usuário insira um número diferente, o programa deverá solicitar novamente uma entrada do usuário
 
 
+
+
+let entrada;
+
+do {
+  entrada = prompt("Digite 1 ou 0:");
+} while (entrada !== "1" && entrada !== "0");
+
+alert("Você digitou " + entrada + ". Obrigado!");
