a2e56a44c8ee565fdfaa69e06fe5c0b1e6ff7ad3
vchavesz <viniciuschaves1007@gmail.com>
2025-09-25 19:42:09 -0300

    aaaa

--- exercicios-aluno/Vinicius/exer10.js
+++ exercicios-aluno/Vinicius/exer10.js
@@ -1,20 +1,37 @@
 //1-3 horas por dia = amarelo
 //3 Horas ou mais por dia = vermelho
 
-let usoDiasDaSemana = [1, 2 , 2,4, 5, 1.2, 0.5, 12]; // Domingo, Segunda..
+let usoDiasDaSemana = [2, 2.4, 5, 1.2, 0.5];
+let diasDaSemana = [
+	'Segunda-feira',
+	'Terça-feira  ',
+	'Quarta-feira ',
+	'Quinta-feira ',
+	'Sexta-feira  ',
+];
 
+// Determinar se o uso do dia é verde, amarelo ou vermelho
 const indicadorDeUso = (numeroDeHoras) => {
-   if (numeroDeHoras <= 1 ) {
-    return ''
- } else if (numeroDeHoras <= 3 ) {
-    return '';
- } else {
-    return ''
- }   
+	if ( numeroDeHoras <= 1 ) {
+		return '🟢';
+	} else if ( numeroDeHoras <= 3 ) {
+		return '🟡';
+	} else {
+		return '🔴';
+	}
 }
 
-console.log(typeof usoDiasDaSemana); //
-console.log(typeof usoDiasDaSemana(0)); // number
-console.log(typeof indicadorDeUso); //function
+for(let contador = 0; contador < usoDiasDaSemana.length ; contador++) {
+	let farol = indicadorDeUso(usoDiasDaSemana[contador]);
+	let dia = diasDaSemana[contador];
+	let horas = usoDiasDaSemana[contador];
+	// let mensagem =  'hora de uso';
 
-usoDiasDaSemana
+	// if ( horas > 1 ) {
+	// 	mensagem = 'horas de uso';
+	// }
+
+	let mensagem = (horas > 1) ? 'horas de uso' : 'hora de uso'; // Operador ternário
+
+	console.log(`${farol} 📅 ${dia} | ${horas.toFixed(2)} ${mensagem}`);
+}
